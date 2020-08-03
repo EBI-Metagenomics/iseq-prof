e644116b3154aa749027202888f72a045b28177e
Danilo Horta <danilo.horta@pm.me>
2020-08-03 18:38:16 +0100

    +files

---
+++ iseq_profmark/__init__.py
@@ -0,0 +1,6 @@
+from . import typing
+from ._confusion import ConfusionMatrix
+from ._genbank import GenBank
+from ._profmark import ProfMark
+
+__all__ = ["genbank", "ConfusionMatrix", "ProfMark", "GenBank", "typing"]
---
+++ iseq_profmark/typing.py
@@ -0,0 +1,3 @@
+__all__ = ["Sample"]
+
+from ._profmark import Sample
