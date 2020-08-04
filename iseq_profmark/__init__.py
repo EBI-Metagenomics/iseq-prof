4f105de6b19be61a33f1d0f627ff38147eba0881
Danilo Horta <danilo.horta@pm.me>
2020-08-04 13:29:05 +0100

    isort

--- iseq_profmark/__init__.py
+++ iseq_profmark/__init__.py
@@ -1,7 +1,22 @@
+from importlib import import_module as _import_module
+
 from . import typing
 from ._confusion import ConfusionMatrix
 from ._genbank import GenBank
 from ._profmark import ProfMark
 from ._testit import test
 
-__all__ = ["genbank", "ConfusionMatrix", "ProfMark", "GenBank", "typing", "test"]
+try:
+    __version__ = getattr(_import_module("iseq_profmark._version"), "version", "x.x.x")
+except ModuleNotFoundError:
+    __version__ = "x.x.x"
+
+__all__ = [
+    "genbank",
+    "ConfusionMatrix",
+    "ProfMark",
+    "GenBank",
+    "typing",
+    "test",
+    "__version__",
+]
