497aad803096b838f614357cdf0227867c08f1ef
Danilo Horta <danilo.horta@pm.me>
2020-08-04 12:53:22 +0100

    testit

--- iseq_profmark/__init__.py
+++ iseq_profmark/__init__.py
@@ -2,5 +2,6 @@ from . import typing
 from ._confusion import ConfusionMatrix
 from ._genbank import GenBank
 from ._profmark import ProfMark
+from ._testit import test
 
-__all__ = ["genbank", "ConfusionMatrix", "ProfMark", "GenBank", "typing"]
+__all__ = ["genbank", "ConfusionMatrix", "ProfMark", "GenBank", "typing", "test"]
