5267b7c6fb02d6fcf33dc3c85a3e39edc200172d
Danilo Horta <danilo.horta@gmail.com>
2020-09-25 12:29:22 +0100

    plot_prof_hits working fine

--- iseq_prof/_cli/_plot_prof_hits.py
+++ iseq_prof/_cli/_plot_prof_hits.py
@@ -1,8 +1,6 @@
-import fileinput
 from collections import defaultdict
-from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict
+from typing import Dict, Set
 
 import click
 import hmmer
@@ -12,13 +10,8 @@ import pandas as pd
 import plotly.express as px
 from tqdm import tqdm
 
-# from .._profiling import Profiling
-
-PERCENTILE = 99
-
-root = Path("output")
-output_fp = "output_1e-10.gff"
-domtbl_fp = "domtblout.txt"
+from .._profiling import Profiling
+from ..solut_space import Sample
 
 __all__ = ["plot_prof_hits"]
 
@@ -31,76 +24,127 @@ __all__ = ["plot_prof_hits"]
     ),
 )
 @click.argument(
-    "outdir",
+    "output",
     type=click.Path(
-        exists=False, dir_okay=True, file_okay=False, writable=True, resolve_path=True
+        exists=False, dir_okay=False, file_okay=True, writable=True, resolve_path=True
     ),
 )
 @click.option(
-    "--output-filename",
-    help="Output filename.",
+    "--gff-filename",
+    help="GFF filename.",
     type=str,
     default="output.gff",
 )
+@click.option(
+    "--percentile",
+    help="Percentile.",
+    type=int,
+    default=100,
+)
+@click.option(
+    "--min-hmmer-hits",
+    help="Minimum number of hmmer hits.",
+    type=int,
+    default=0,
+)
 def plot_prof_hits(
     experiment: str,
-    outdir: str,
-    output_filename: str,
+    output: str,
+    gff_filename: str,
+    percentile: int,
+    min_hmmer_hits: int,
 ):
     """
     Show profile hits.
     """
-    pass
-    # root = Path(experiment)
-    # prof = Profiling(root)
+    root = Path(experiment)
+    prof = Profiling(root)
 
+    iseq_count: Dict[str, int] = defaultdict(lambda: 0)
+    hmmer_count: Dict[str, int] = defaultdict(lambda: 0)
 
-if __name__ == "__main__":
+    for acc in tqdm(prof.accessions):
+        fp = root / acc / gff_filename
+        if not fp.exists():
+            click.echo(f"{fp} not found.", err=True)
+            continue
 
-    @dataclass
-    class Profile:
-        name: str
-        accession: str
+        hprofs = hmmer_profiles(root / acc / "domtblout.txt")
+        iprofs = iseq_profiles(root / acc / gff_filename)
 
-        def __hash__(self):
-            return hash(self.accession)
+        matches = hprofs & iprofs
 
-    def update_iseq_count(count: Dict[Profile, int], output_fp: Path):
-        gff = iseq.gff.read(output_fp)
-        gff.ravel()
-        df = gff.dataframe
-        for row in df.itertuples(False):
-            prof = Profile(row.att_Profile_name, row.att_Profile_acc)
-            count[prof] = count[prof] + 1
+        for prof in hprofs:
+            hmmer_count[prof.profile] += 1
 
-    def update_hmmer_count(count: Dict[Profile, int], domtbl_fp: Path):
-        for row in hmmer.read_domtbl(domtbl_fp):
-            prof = Profile(row.target.name, row.target.accession)
-            count[prof] = count[prof] + 1
+        for prof in matches:
+            iseq_count[prof.profile] += 1
 
-    iseq_count: Dict[Profile, int] = defaultdict(lambda: 0)
-    hmmer_count: Dict[Profile, int] = defaultdict(lambda: 0)
-
-    for line in tqdm(fileinput.input()):
-        acc = line.strip()
-        update_iseq_count(iseq_count, root / acc / output_fp)
-        update_hmmer_count(hmmer_count, root / acc / domtbl_fp)
-
-    data = [(key.name, val, "iseq") for key, val in iseq_count.items()]
-    data += [(key.name, val, "hmmer") for key, val in hmmer_count.items()]
+    data = [(key, val, "iseq-match") for key, val in iseq_count.items()]
+    data += [(key, val, "hmmer") for key, val in hmmer_count.items()]
 
     df = pd.DataFrame(data, columns=["profile", "count", "method"])
     df["profile"] = df["profile"].astype(str)
     df["count"] = df["count"].astype(int)
     df["method"] = df["method"].astype(str)
 
-    def filter_low_hits(df: pd.DataFrame):
-        thr = int(np.percentile(df["count"], PERCENTILE))
-        profs = df[df["count"] > thr]["profile"].unique()
-        df = df[df["profile"].isin(profs)]
-        return df
+    df["ratio"] = 1.0
+    df.set_index(["profile", "method"], inplace=True)
+    for i in df.index:
+        df.loc[i, "ratio"] = df.loc[i, "count"] / df.loc[(i[0], "hmmer"), "count"]
 
-    df = filter_low_hits(df)
+    df.reset_index(drop=False, inplace=True)
 
-    fig = px.bar(df, x="profile", y="count", color="method", barmode="group")
-    fig.write_html("profile_hits.html")
+    profs = count_interesting_profiles(df, min_hmmer_hits)
+    df = df[df["profile"].isin(profs)]
+
+    profs = ratio_interesting_profiles(df, percentile)
+    df = df[df["profile"].isin(profs)]
+
+    df.sort_values("ratio", inplace=True, kind="mergesort")
+
+    fig = px.bar(
+        df,
+        x="profile",
+        y="count",
+        color="method",
+        barmode="group",
+        hover_data=["profile", "count", "ratio"],
+    )
+    outpath = Path(output)
+    if outpath.suffix == ".html":
+        fig.write_html(str(outpath))
+    else:
+        fig.write_image(str(outpath))
+
+
+def hmmer_profiles(domtbl_fp: Path) -> Set[Sample]:
+    samples = []
+    for row in hmmer.read_domtbl(domtbl_fp):
+        target = row.query.name.partition("|")[0]
+        samples.append(Sample(row.target.name, target))
+    return set(samples)
+
+
+def iseq_profiles(output_fp: Path) -> Set[Sample]:
+    gff = iseq.gff.read(output_fp)
+    gff.ravel()
+    df = gff.dataframe
+    samples = []
+    for row in df.itertuples(False):
+        target = row.seqid.partition("|")[0]
+        samples.append(Sample(row.att_Profile_name, target))
+    return set(samples)
+
+
+def ratio_interesting_profiles(df: pd.DataFrame, percentile: int):
+    df = df[df["method"] == "iseq-match"]
+    thr = np.percentile(df["ratio"], percentile)
+    df = df[df["ratio"] <= thr]
+    return df["profile"].unique()
+
+
+def count_interesting_profiles(df: pd.DataFrame, min_hmmer_hits: int):
+    df = df[df["method"] == "hmmer"]
+    df = df[df["count"] >= min_hmmer_hits]
+    return df["profile"].unique()
